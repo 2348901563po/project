c6d83b18e0584a39ae4282c089cf5ffdba019a30
2348901563po <72119878+2348901563po@users.noreply.github.com>
2023-03-21 01:38:31 -0500

    styles

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -6,6 +6,7 @@ const Search = () =>{
     return(
         <div className={styles.container}>
             <div className={styles.name}>
+                <div className={styles.title}>Search</div>
                 <form>
                     <label htmlFor="search">
                     <input name='search' type='text' className={styles.search}></input></label>
